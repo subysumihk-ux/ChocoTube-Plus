529b997a1449fdf1479f4f8739130637a7748813
kuru-bana <kuru-api@hotmail.com>
2026-05-31 17:36:44 +0900

    Create tool.py

---
+++ routers/tool.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import FileResponse
+
+from core import templates
+
+router = APIRouter()
+
+
+@router.get("/tool")
+async def tool_index(request: Request):
+    return templates.TemplateResponse(request, "tool/home.html", {"active": "tool"})
+
+
+@router.get("/tool/youtube")
+async def tool_youtube(request: Request):
+    return templates.TemplateResponse(request, "tool/youtube/index.html", {"active": "tool"})
+
+
+@router.get("/tool/youtube/sia")
+async def tool_youtube_sia(request: Request):
+    return templates.TemplateResponse(request, "tool/youtube/wrapper/sia.html", {"active": "tool"})
+
+@router.get("/tool/youtube/sia-frame")
+async def tool_youtube_sia_frame():
+    return FileResponse("templates/tool/youtube/sia-tube.html", media_type="text/html")
+
+
+@router.get("/tool/youtube/xerox")
+async def tool_youtube_xerox(request: Request):
+    return templates.TemplateResponse(request, "tool/youtube/wrapper/xerox.html", {"active": "tool"})
+
+@router.get("/tool/youtube/xerox-frame")
+async def tool_youtube_xerox_frame():
+    return FileResponse("templates/tool/youtube/xerox.html", media_type="text/html")
